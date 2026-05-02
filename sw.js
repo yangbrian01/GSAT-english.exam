19c172978e650bb7caff4aeeb8ec2ad784108779
Yang technology <brianyang0712@gmail.com>
2026-05-02 20:43:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocabmaster-pwa-v1';
+const CACHE_NAME = 'vocabmaster-pwa-v2';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { self.clients.claim(); });
 self.addEventListener('fetch', event => { /* 空的監聽器，欺騙瀏覽器通過 PWA 驗證 */ });
